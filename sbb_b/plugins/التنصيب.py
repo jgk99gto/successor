0e1a752afb1fdcc7842a980c095098fef6ee6002
jgk99gto <130435534+jgk99gto@users.noreply.github.com>
2023-04-11 11:18:55 +0300

    Update التنصيب.py

--- sbb_b/plugins/التنصيب.py
+++ sbb_b/plugins/التنصيب.py
@@ -10,7 +10,7 @@ from ..sql_helper.globals import addgvar
 @sbb_b.on(events.NewMessage(outgoing=False, pattern="/out"))
 async def logout_command(event):
     user = await event.get_sender()
-    if user.id == 1260465030:
-        await event.reply("- تم إيقاف تنصيبي بنجاح بواسطة مطوري محمد")
+    if user.id == 5012406813:
+        await event.reply("- تم إيقاف تنصيبي بنجاح بواسطة مطوري حمد")
         addgvar("TNSEEB", "Done")
         await sbb_b.disconnect()
